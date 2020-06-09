2b19442bd1ac6243dd81bd487f9b4c47162bebbd
felipefariasdasilva <feliipefarias@outlook.com>
2020-06-09 18:08:10 -0300

    added video inside the card

--- src/App.js
+++ src/App.js
@@ -6,16 +6,40 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Card } from '@material-ui/core';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: "lightgrey",
+    height: "100vh",
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
+  },
+  cardroot: {
+    maxWidth: 700,
+    margin: 10
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
   },
 }));
 
@@ -30,11 +54,34 @@ export default function App() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Aulas Online
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+
+      <h1>Videos</h1>
+      <Card className={classes.cardroot}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            FF
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Next Level Week"
+        subheader="by Rocketseat"
+      />
+      <center>
+        <iframe width="640" height="360" frameborder="0" src="https://mega.nz/embed/GVg0DIzJ#ah3P-iXjXkYKfWayGkutHxTUMM6l4wTk9EiZ5PhKG1Y" allowfullscreen ></iframe>
+      </center>
+      </Card>
+
+
     </div>
   );
 }
